d90e5e6979396a08a00087317e098310865e6d7a
Kyle Filz <kfilz@live.ca>
2012-05-08 15:55:32 -0500

    Shorter faster jekyll script

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -1,43 +1,27 @@
 #!/bin/bash
+
 # Release Repository
-sheets=$PWD"/stylesheets"
-function relative {
-	source="`cd $1; pwd`"
-	target=$sheets
-	common_part=$source
-	back=
-	while [ "${target#$common_part}" = "${target}" ]; do
-	common_part=$(dirname "${common_part}")
-	back="../${back}"
-	done
-	
-	echo -e "${back}${target#$common_part/}"
-}
 for DIR in $(find ./releases -type d); do
   (
-    echo -e "<html>\n<link rel=\"stylesheet\" type=\"text/css\" href=\""
-	relative $DIR
-	echo -e "/stylesheet.css\" media=\"screen\" />\n<link rel=\"stylesheet\" type=\"text/css\" href=\""
-	relative $DIR
-	echo -e "/pygment_trac.css\" media=\"screen\" />\n<link rel=\"stylesheet\" type=\"text/css\" href=\""
-	relative $DIR
-	echo -e "/print.css\" media=\"print\" /><body>\n<header><div class=\"container\"><h1>Spice-King's Maven Releases Repository</h1></div></header>\n<div class=\"container\"><section id=\"main_content\"><pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
-    echo -e "</pre></section></div>\n</body>\n</html>"
-  ) > "${DIR}/index.html"
+	echo "---"
+	echo "layout: default"
+	echo "title: Releases"
+	echo "---"
+	echo "<pre>"
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.markdown" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
+	echo "</pre>"
+  ) > "${DIR}/index.markdown"
 done
 
 # Snapshot Repository
 for DIR in $(find ./snapshots -type d); do
   (
-    echo -e "<html>\n<link rel=\"stylesheet\" type=\"text/css\" href=\""
-	relative $DIR
-	echo -e "/stylesheet.css\" media=\"screen\" />\n<link rel=\"stylesheet\" type=\"text/css\" href=\""
-	relative $DIR
-	echo -e "/pygment_trac.css\" media=\"screen\" />\n<link rel=\"stylesheet\" type=\"text/css\" href=\""
-	relative $DIR
-	echo -e "/print.css\" media=\"print\" /><body>\n<header><div class=\"container\"><h1>Spice-King's Maven Snapshots Repository</h1></div></header>\n<div class=\"container\"><section id=\"main_content\"><pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
-    echo -e "</pre></section></div>\n</body>\n</html>"
-  ) > "${DIR}/index.html"
+	echo "---"
+	echo "layout: default"
+	echo "title: Snapshots"
+	echo "---"
+	echo "<pre>"
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.markdown" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
+	echo "</pre>"
+  ) > "${DIR}/index.markdown"
 done
